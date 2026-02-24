74df2578a30cd869ad96ee2e960a4d8dfbefda03
susmithametta9-debug <susmithametta9@gmail.com>
2026-02-24 12:14:20 -0500

    created the alternate tablet layout through device manager and its working

--- app/src/main/java/edu/temple/fancyscreen/MainActivity.kt
+++ app/src/main/java/edu/temple/fancyscreen/MainActivity.kt
@@ -4,10 +4,9 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
@@ -16,50 +15,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<ImageView>(R.id.profile_photo).setImageResource(R.drawable.istockphoto)
+        val profilePhoto = findViewById<ImageView>(R.id.profile_photo)
+        val nameTextView = findViewById<TextView>(R.id.name)
+        val emailTextView = findViewById<TextView>(R.id.email)
+        val extensionTextView = findViewById<TextView>(R.id.extension)
+        val departmentTextView = findViewById<TextView>(R.id.department)
+        val supervisorTextView = findViewById<TextView>(R.id.supervisor)
+        val directReportsRecyclerView = findViewById<RecyclerView>(R.id.directReportsRecyclerView)
 
-        findViewById<TextView>(R.id.name).setText("Michelle Townsend")
+        profilePhoto.setImageResource(R.drawable.istockphoto)
+        nameTextView.text = "Michelle Townsend"
+        emailTextView.text = "mt@ourcompany.com"
+        emailTextView.setTextColor(Color.BLUE)
+        extensionTextView.text = "2253"
+        departmentTextView.text = "Design"
+        supervisorTextView.text = "Gail Davers"
 
-        with(findViewById<TextView>(R.id.email)) {
-            text = "mt@ourcompany.com"
-            setTextColor(Color.BLUE)
-        }
+        val staffList = arrayOf(
+            "Kate Sacloff",
+            "Andrew Klein",
+            "Maria Ortega",
+            "Brent Stevenson",
+            "Daniel Cho",
+            "Jorge Gomez"
+        )
 
-        findViewById<TextView>(R.id.extension).text = "2253"
-
-        findViewById<TextView>(R.id.department).text = "Design"
-
-        findViewById<TextView>(R.id.supervisor).text = "Gail Davers"
-
-        with (findViewById<RecyclerView>(R.id.directReportsRecyclerView)) {
-            adapter = RecyclerViewAdapter(
-                arrayOf(
-                    "Kate Sacloff",
-                    "Andrew Klein",
-                    "Maria Ortega",
-                    "Brent Stevenson",
-                    "Daniel Cho",
-                    "Jorge Gomez"
-                )
-            )
-            layoutManager = LinearLayoutManager(this@MainActivity)
-        }
+        directReportsRecyclerView.layoutManager = GridLayoutManager(this, resources.getInteger(R.integer.grid_columns))
+        directReportsRecyclerView.adapter = RecyclerViewAdapter(staffList)
     }
 }
 
 class RecyclerViewAdapter (private val staffList: Array<String>) : RecyclerView.Adapter<RecyclerViewAdapter.StaffListViewHolder>() {
-    class StaffListViewHolder(val textView: TextView) : ViewHolder(textView) {
-
-    }
+    class StaffListViewHolder(val textView: TextView) : ViewHolder(textView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StaffListViewHolder {
-        return StaffListViewHolder(
-            TextView(parent.context).apply {
-                textSize = 22f
-                setTextColor(Color.BLUE)
-                setPadding(50,8,0,8)
-            }
-        )
+        val textView = TextView(parent.context).apply {
+            textSize = 22f
+            setTextColor(Color.BLUE)
+            setPadding(50, 8, 0, 8)
+        }
+        return StaffListViewHolder(textView)
     }
 
     override fun getItemCount() = staffList.size
